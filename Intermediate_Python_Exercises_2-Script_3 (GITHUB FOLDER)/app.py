2cee2223466cb39d587253a1543016cc265c6388
hassanchug <89851757+hassanchug@users.noreply.github.com>
2022-02-01 23:25:54 -0500

    Intermediate Python Exercises 2 - Script 3 commit

---
+++ Intermediate_Python_Exercises_2-Script_3 (GITHUB FOLDER)/app.py
@@ -0,0 +1,9 @@
+# Group Members: Ivory Steed, Paul Thottappilly, Hassan Chugtai, Tony Le, Shekar Balasubramaniam
+
+# Links below indicate help found online
+
+import requests
+
+r = requests.get('https://www.charlotte.edu/')
+
+print(r.text)
