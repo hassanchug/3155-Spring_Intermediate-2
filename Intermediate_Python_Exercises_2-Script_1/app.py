bf6f71fb29f166da427e875609578c117981d609
hassanchug <89851757+hassanchug@users.noreply.github.com>
2022-02-01 23:33:57 -0500

    added comments

--- Intermediate_Python_Exercises_2-Script_1/app.py
+++ Intermediate_Python_Exercises_2-Script_1/app.py
@@ -1,9 +1,10 @@
 # Group Members: Ivory Steed, Paul Thottappilly, Hassan Chughtai, Tony Le, Shekar Balasubramaniam
 
-
+# import the Student class
 from student import Student
-
+# instantiate with a name and gpa
 studentRep = Student("Oliver", 2.45)
+# stores both as an instance by calling the report_gpa method
 report = studentRep.report_gpa()
-
+# prints the instance calling the method
 print(report)
