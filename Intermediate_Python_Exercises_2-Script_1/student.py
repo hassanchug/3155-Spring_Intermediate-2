0cafa6f87c6f8e11c8c138d0fc6ff2c3c85715f7
hassanchug <89851757+hassanchug@users.noreply.github.com>
2022-02-01 23:35:52 -0500

    added comments

--- Intermediate_Python_Exercises_2-Script_1/student.py
+++ Intermediate_Python_Exercises_2-Script_1/student.py
@@ -3,10 +3,11 @@
 
 
 class Student:
-
+    
+    # constructor that takes in a name and GPA and stores them both as instance fields
     def __init__(self, name, gpa):
         self.name = name
         self.gpa = gpa
-    
+    # creates report_gpa method that returns a string
     def report_gpa(self):
         return(self.name + " has a GPA of " + str(self.gpa))
